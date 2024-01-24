f44e5e16fa0646ea6acde8e9c9e4635e6cb605a0
Mannat Jaiswal <mannatjaiswal03@gmail.com>
2024-01-24 05:37:19 +0530

    Implemented IndexedStack, used sliver in the homepage(incomplete),added empty widgets of 'sell items','wishlist' and 'my account' screens

---
+++ lib/ui/sell_products.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class SellProducts extends StatefulWidget {
+  const SellProducts({super.key});
+
+  @override
+  State<SellProducts> createState() => _SellProductsState();
+}
+
+class _SellProductsState extends State<SellProducts> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Sell Products"),
+      ),
+    );
+  }
+}

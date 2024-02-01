22ef4d0db97ff030d4e166dded0ffcc8c44a5b8a
Mannat Jaiswal <mannatjaiswal03@gmail.com>
2024-02-01 14:06:11 +0530

    added crud operation controllers in the server dir

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -3,9 +3,51 @@ const Product = require("../models/product.js");
 module.exports.index = async (req, res) => {
   try {
     let result = await Product.find({});
-    res.send(result);
-    console.log(result);
+    res.render("products/index.ejs", { products: result });
   } catch (err) {
     console.log(err);
   }
+};
+
+module.exports.renderNew = (req, res) => {
+  res.render("products/new.ejs");
+};
+
+module.exports.postNew = async (req, res) => {
+  let result = new Product(req.body.product);
+
+  await result
+    .save()
+    .then(() => {
+      console.log("product added to DB");
+    })
+    .catch((err) => {
+      throw err;
+    });
+  res.redirect("/products");
+};
+
+module.exports.showProduct = async (req, res) => {
+  let { id } = req.params;
+  let result = await Product.findById(id);
+  res.render("products/show.ejs", { product: result });
+};
+
+module.exports.deleteProduct = async (req, res) => {
+  let { id } = req.params;
+  let result = await Product.findByIdAndDelete(id);
+  res.redirect("/products");
+};
+
+module.exports.renderEditProductDetails = async (req, res) => {
+  let { id } = req.params;
+  let result = await Product.findById(id);
+  res.render("products/edit.ejs", { product: result });
+};
+
+module.exports.editProductDetails = async (req, res) => {
+  let { id } = req.params;
+  let result = await Product.findByIdAndUpdate(id, req.body.product);
+  console.log(result);
+  res.redirect("/products");
 };
--- server/routes/products.js
+++ server/routes/products.js
@@ -3,6 +3,19 @@ const router = express.Router({ mergeParams: true });
 const Product = require("../models/product");
 const productsController = require("../controllers/products");
 
-router.route("/").get(productsController.index);
+router
+  .route("/")
+  .get(productsController.index)
+  .post(productsController.postNew);
+
+router.route("/new").get(productsController.renderNew);
+
+router
+  .route("/:id")
+  .get(productsController.showProduct)
+  .delete(productsController.deleteProduct)
+  .patch(productsController.editProductDetails);
+
+router.route("/:id/edit").get(productsController.renderEditProductDetails);
 
 module.exports = router;
